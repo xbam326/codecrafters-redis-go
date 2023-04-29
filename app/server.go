e55f87e45021eaba6e95bbc3480296108ddc2465
Masakuni <xxbambooxx@icloud.com>
2023-04-29 16:08:52 +0900

    3rd stage

--- app/server.go
+++ app/server.go
@@ -13,20 +13,20 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
-	
-	buf := make([]byte, 1024)
-	if _, err := conn.Read(buf); err != nil {
-		fmt.Println("error reading from client: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		defer conn.Close()
+		
+		buf := make([]byte, 1024)
+		if _, err := conn.Read(buf); err != nil {
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
 
-	// Let's ignore the client's input for now and hardcode a response.
-	// We'll implement a proper Redis Protocol parser in later stages.
-	conn.Write([]byte("+PONG\r\n"))
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
