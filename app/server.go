9ffb4e28867be7fb959ab7a2e55e3ca5841c72c9
Masakuni <xxbambooxx@icloud.com>
2023-04-29 15:52:51 +0900

    2nd stage

--- app/server.go
+++ app/server.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+	
+	buf := make([]byte, 1024)
+	if _, err := conn.Read(buf); err != nil {
+		fmt.Println("error reading from client: ", err.Error())
+		os.Exit(1)
+	}
+
+	// Let's ignore the client's input for now and hardcode a response.
+	// We'll implement a proper Redis Protocol parser in later stages.
+	conn.Write([]byte("+PONG\r\n"))
 }
